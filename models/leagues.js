a6e83cbb5e097c5203e031c2e3f01f8d5e69014f
qwer946 <khalid.ali.987@gmail.com>
2022-06-01 21:29:29 -0500

    wip

---
+++ models/leagues.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    name: "Premier League",
+    description: "Who is going to win the premier league title?",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ22Td-P39gtKXCEWGTbjgg_P2t1DtaibROTTOUl6s&usqp=CAE&s",
+  },
+  {
+    name: "LaLiga",
+    description:
+      "Who is better messi or ronaldo, when they where at this league?",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS2HSKDmA10a71vgbus1oO3Qig5rxqDq8etjNXdA8OHg&s",
+  },
+  {
+    name: "Serie A",
+    description: "Who will enter the champions league?",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVutY6nvkz6I2Wi7bFZRDenZbdrw0prz4Yn8Olhm-4nw&s",
+  },
+  {
+    name: "Bundesliga",
+    description: "Is this league really a farmers league?",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkC1sDSwiFr7C_NnWA77qiYRefFenY8WBAfQrxcJI&usqp=CAE&s",
+  },
+];
