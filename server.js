edc6df9e0b4f0a94d792b9770b7c3c21e0d3f2cf
qwer946 <khalid.ali.987@gmail.com>
2022-06-01 21:11:20 -0500

    wip

--- server.js
+++ server.js
@@ -1,61 +1,101 @@
 require("dotenv").config();
-
-//___________________
-//Dependencies
-//___________________
-const express = require("express");
-const methodOverride = require("method-override");
 const mongoose = require("mongoose");
+const Club = require("./models/Soccer");
+const express = require("express");
 const app = express();
-const db = mongoose.connection;
-//___________________
-//Port
-//___________________
-// Allow use of Heroku's port or your own local port, depending on the environment
-const PORT = process.env.PORT || 3000;
+const methodOverride = require("method-override");
+// const clubs = require("./models/leagues");
 
-//___________________
-//Database
-//___________________
-// How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = 3000;
 
-// Connect to Mongo &
-// Fix Depreciation Warnings from Mongoose
-// May or may not need these depending on your Mongoose version
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
+// Database Connection
+mongoose.connect(process.env.DATABASE_URL, {
   useUnifiedTopology: true,
 });
+// Database Connection Error/Success
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err.message + " is mongo not running?"));
+db.on("connected", () => console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
 
-// Error / success
-db.on("error", (err) => console.log(err.message + " is mongod not running?"));
-db.on("connected", () => console.log("mongod connected: ", MONGODB_URI));
-db.on("disconnected", () => console.log("mongod disconnected"));
+app.use(methodOverride("_method"));
+//static files
+app.use(express.static(__dirname + "/public"));
 
-//___________________
-//Middleware
-//___________________
+// MIDDLEWARE & BODY PARSER
+app.use(express.urlencoded({ extended: true }));
 
-//use public folder for static assets
-app.use(express.static("public"));
-
-// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false })); // extended: false - does not allow nested objects in query strings
-app.use(express.json()); // returns middleware that only parses JSON - may or may not need it depending on your project
-
-//use method override
-app.use(methodOverride("_method")); // allow POST, PUT and DELETE from a form
-
-//___________________
-// Routes
-//___________________
-//localhost:3000
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// Index
+app.get("/clubs", (req, res) => {
+  Club.find({}, (err, allClubs) => {
+    res.render("index.ejs", {
+      clubs: allClubs,
+    });
+  });
 });
 
-//___________________
-//Listener
-//___________________
-app.listen(PORT, () => console.log("express is listening on:", PORT));
+//NEW
+app.get("/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+// SEED DATA!
+// const clubLeagues = require("./models/leagues");
+// app.get("/clubs/leagues", (req, res) => {
+//   Club.create(clubLeagues, (error, data) => {
+//     res.redirect("/clubs");
+//   });
+// });
+
+// // SHOW
+app.get("/clubs/:id", (req, res) => {
+  Club.findById(req.params.id, (err, foundClub) => {
+    res.render("show.ejs", {
+      club: foundClub,
+    });
+  });
+});
+// UPDATE
+app.put("/clubs/:id", (req, res) => {
+  if (req.body.completed === "on") {
+    req.body.completed = true;
+  } else {
+    req.body.completed = false;
+  }
+  Club.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    },
+    (error, updatedClub) => {
+      res.redirect(`/clubs/${req.params.id}`);
+    }
+  );
+});
+//CREATE
+app.post("/clubs", (req, res) => {
+  Club.create(req.body, (err, createdStore) => {
+    if (err) console.log(err);
+    res.redirect("/clubs");
+  });
+});
+
+//EDIT
+app.get("/clubs/:id/edit", (req, res) => {
+  Club.findById(req.params.id, (error, foundClub) => {
+    res.render("edit.ejs", {
+      club: foundClub,
+    });
+  });
+});
+// DELETE
+app.delete("/clubs/:id", (req, res) => {
+  Club.findByIdAndRemove(req.params.id, (err, deletedClub) => {
+    res.redirect("/clubs");
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
